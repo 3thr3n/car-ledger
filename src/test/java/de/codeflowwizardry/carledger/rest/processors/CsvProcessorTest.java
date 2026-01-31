bfb04cc3484f577a383970c7b0b16055585cf5b6
3thr3n <info@codeflowwizardry.de>
2026-01-31 19:25:17 +0100

    🐛 Fix csv import using always 19% vat

--- src/test/java/de/codeflowwizardry/carledger/rest/processors/CsvProcessorTest.java
+++ src/test/java/de/codeflowwizardry/carledger/rest/processors/CsvProcessorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
+import java.math.BigInteger;
 import java.net.URL;
 
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,7 @@ class CsvProcessorTest
 
 		// when/then
 		assertThrows(NullPointerException.class,
-				() -> csvProcessor.processCsv(new File(url.toURI()), null, null, false));
+				() -> csvProcessor.processCsv(new File(url.toURI()), null, null, false, BigInteger.valueOf(19)));
 	}
 
 	@Test
@@ -58,6 +59,7 @@ class CsvProcessorTest
 		URL url = Thread.currentThread().getContextClassLoader().getResource("csv/import_1.csv");
 
 		// when/then
-		assertDoesNotThrow(() -> csvProcessor.processCsv(new File(url.toURI()), new CsvOrder(), carEntity, false));
+		assertDoesNotThrow(() -> csvProcessor.processCsv(new File(url.toURI()), new CsvOrder(), carEntity, false,
+				BigInteger.valueOf(19)));
 	}
 }
